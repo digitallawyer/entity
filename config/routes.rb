5ef651e4202164abd85cc07107816cc91767e919
“digitallawyer” <pieter@lawgives.com>
2014-01-04 15:37:00 -0800

    enabling version control

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Entity::Application.routes.draw do
+  resources :corporations
+
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
