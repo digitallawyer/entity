ca6a38da4f7c713b0f404307cf166948ae2a5c83
“digitallawyer” <pieter@lawgives.com>
2014-01-04 17:16:48 -0800

    add and style basic corporation models

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140104232917) do
+ActiveRecord::Schema.define(version: 20140104233427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "corporations", force: true do |t|
+    t.string   "name"
+    t.string   "extension"
+    t.string   "share_number"
+    t.string   "share_value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
